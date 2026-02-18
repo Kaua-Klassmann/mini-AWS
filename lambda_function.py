89850aecab3e1e382d8ab2d274556e04d743da84
Kauã Klassmann <kaua.klassmann661@gmail.com>
2026-02-17 21:02:04 -0300

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,25 @@
+import json
+
+def lambda_handler(event, context):
+    body = event.get('body')
+
+    if body is None:
+        message = event.get('message')
+    else:
+        if isinstance(body, str):
+            body = json.loads(body)
+            
+        message = body.get('message')
+
+    if message is None:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('No message provided')
+        }
+
+    message = message.upper()[::-1]
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(message)
+    }
